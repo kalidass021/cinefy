c694e0da8f45142088fc64498263d5c4f5731839
Kalidass <kalidass.k021@gmail.com>
2024-11-06 15:59:48 +0530

    create update current user profile

--- api/src/controllers/userController.js
+++ api/src/controllers/userController.js
@@ -1,3 +1,4 @@
+import bcrypt from 'bcryptjs';
 import User from '../models/User.js';
 import customError from '../utils/customError.js';
 
@@ -16,16 +17,47 @@ export const getCurrentUserProfile = async (req, res, next) => {
     const user = await User.findById(req.user._id).select('-password');
 
     if (!user) {
-        return next(customError(404, 'User not found'));
+      return next(customError(404, 'User not found'));
     }
 
     res.status(200).json({
-        _id: user._id,
-        username: user.username,
-        email: user.email,
-    })
+      _id: user._id,
+      username: user.username,
+      email: user.email,
+    });
   } catch (err) {
     console.error(`Error in getCurrentUserProfile controller ${err}`);
+    next(err);
+  }
+};
+
+export const updateCurrentUserProfile = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.user._id);
+
+    if (!user) {
+      return next(customError(404, 'User not found'));
+    }
+
+    // if user
+    user.username = req.body.username || user.username;
+    user.email = req.body.email || user.email;
+
+    if (req.body.password) {
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(req.body.password, salt);
+      user.password = hashedPassword;
+    }
+    const updatedUser = await user.save();
+
+    res.status(200).json({
+      _id: updatedUser._id,
+      username: updatedUser.username,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+    });
+  } catch (err) {
+    console.error(`Error in updateCurrentUserProfile controller ${err}`);
     next(err);
   }
 };
--- api/src/routes/userRoutes.js
+++ api/src/routes/userRoutes.js
@@ -1,12 +1,19 @@
 import { Router } from 'express';
 // controllers
-import { getAllUsers, getCurrentUserProfile } from '../controllers/userController.js';
+import {
+  getAllUsers,
+  getCurrentUserProfile,
+  updateCurrentUserProfile,
+} from '../controllers/userController.js';
 // middlewares
 import { authenticate, authorizeAdmin } from '../middlewares/auth.js';
 
 const router = Router();
 
 router.get('/', authenticate, authorizeAdmin, getAllUsers);
-router.get('/profile', authenticate, getCurrentUserProfile);
+router
+  .route('/profile')
+  .get(authenticate, getCurrentUserProfile)
+  .put(authenticate, updateCurrentUserProfile);
 
 export default router;
