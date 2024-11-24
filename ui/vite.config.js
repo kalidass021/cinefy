b89f98b851a59aba5744ea828e8f80b43acf8bbc
Kalidass <kalidass.k021@gmail.com>
2024-11-24 22:51:45 +0530

    remove proxy server setup from vite config

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -4,13 +4,4 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      // "/api/": "http://localhost:5000/",
-      // "/uploads/": "http://localhost:5000/",
-      // production api
-      "/api/": "https://cinefy-teqc.onrender.com",
-      "/uploads/": "https://cinefy-teqc.onrender.com"
-    }
-  }
 })
