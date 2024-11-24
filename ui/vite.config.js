28ac54a4465a0703b6c92c4aeb50e5872770343a
Kalidass <kalidass.k021@gmail.com>
2024-11-24 23:16:19 +0530

    revoke last chage

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -1,16 +1,7 @@
-import {fileURLToPath} from 'url';
-import path, {dirname} from 'path';
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
 })
