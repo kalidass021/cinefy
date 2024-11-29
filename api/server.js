0b43901557c266b60aeb63a860d5abc2b452fba3
Kalidass <kalidass.k021@gmail.com>
2024-11-29 21:52:26 +0530

    add err.stack to error handling middleware for error trace

--- api/server.js
+++ api/server.js
@@ -49,6 +49,7 @@ app.use('/uploads', express.static(path.join(rootDir + '/uploads')));
 
 // middleware to handle the errors
 app.use((err, req, res, next) => {
+  err.stack && console.error(err.stack);
   const statusCode = err.statusCode || 500;
   const message = err.message || 'Internal Server Error';
   res.status(statusCode).json({
