5724c2966d39de5dc32b0bf5baba9b21ce647d0e
Kalidass <kalidass.k021@gmail.com>
2025-03-15 23:45:11 +0530

    test by adding useKeepAwake to keep render server awake

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,8 +1,11 @@
 import { Outlet } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import NavigationDock from './components/NavigationDock';
+import useKeepAwake from './hooks/useKeepAwake';
+import { BASE_URL } from './config/constants';
 
 const App = () => {
+  useKeepAwake(BASE_URL);
   return (
     <>
       <Toaster position='top-right' reverseOrder={false} />
---
+++ ui/src/hooks/useKeepAwake.js
@@ -0,0 +1,26 @@
+import { useEffect } from 'react';
+
+const useKeepAwake = (API_URL, intervalMinutes = 14) => {
+  useEffect(() => {
+    let timeoutId;
+
+    const pingBackend = async () => {
+      try {
+        await fetch(API_URL);
+        console.log(API_URL);
+        console.info('Pinged backend to keep it awake');
+      } catch (err) {
+        console.error(`Ping failed: ${err.message}`);
+      }
+
+      // schedule the next ping after the previous request completes
+      timeoutId = setTimeout(pingBackend, intervalMinutes * 60 * 1000); // 14 mins
+    };
+
+    pingBackend(); // initial ping
+
+    return () => clearTimeout(timeoutId); // cleanup when component unmounts
+  }, [API_URL, intervalMinutes]);
+};
+
+export default useKeepAwake;
