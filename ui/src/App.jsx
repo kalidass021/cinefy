c9bbd7299e9ff5bb2949c5f861c94efa335d06f2
Kalidass <kalidass.k021@gmail.com>
2025-07-07 23:51:54 +0530

    comment useKeepAwake hook

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,11 +1,11 @@
 import { Outlet } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import { NavigationDock } from './components';
-import useKeepAwake from './hooks/useKeepAwake';
-import { BASE_URL } from './config/constants';
+// import useKeepAwake from './hooks/useKeepAwake';
+// import { BASE_URL } from './config/constants';
 
 const App = () => {
-  useKeepAwake(BASE_URL);
+  // useKeepAwake(BASE_URL);
   return (
     <>
       <Toaster position='top-right' reverseOrder={false} />
