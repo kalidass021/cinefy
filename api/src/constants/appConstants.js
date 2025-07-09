c48df48a38a9ebc0020636da45fcc627f78cc284
Kalidass <kalidass.k021@gmail.com>
2025-07-09 20:03:21 +0530

    save status codes in app constants

---
+++ api/src/constants/appConstants.js
@@ -0,0 +1,16 @@
+export const STATUS_CODES = {
+  OK: 200,
+  Created: 201,
+  Accepted: 202,
+  NoContent: 204,
+  ResetContent: 205,
+  BadRequest: 400,
+  Unauthorized: 401,
+  PaymentRequired: 402,
+  Forbidden: 403,
+  NotFound: 404,
+  MethodNotAllowed: 405,
+  InternalServerError: 500,
+  ServiceUnavailable: 503,
+  HTTPVersionNotSupported: 505,
+};
