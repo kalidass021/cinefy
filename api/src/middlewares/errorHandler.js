4a1fc04e927d511ca7bb25a44381d101360a5739
Kalidass <kalidass.k021@gmail.com>
2025-07-10 22:49:26 +0530

    use status codes in errorHandler

--- api/src/middlewares/errorHandler.js
+++ api/src/middlewares/errorHandler.js
@@ -1,6 +1,8 @@
+import { STATUS_CODES } from "../constants/appConstants";
+
 const errorHandler = (err, req, res, next) => {
   err.stack && console.error(err.stack);
-  const statusCode = err.statusCode || 500;
+  const statusCode = err.statusCode || STATUS_CODES.InternalServerError;
   const message = err.message || err.error || 'Internal Server Error';
   res.status(statusCode).json({
     success: false,
