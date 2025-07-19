eb2d3a96c72e11cdca5f6980de5dfe72c7bc4a35
Kalidass <kalidass.k021@gmail.com>
2025-07-19 14:38:10 +0530

    format api status

--- api/src/utils/apiStatus.js
+++ api/src/utils/apiStatus.js
@@ -1,11 +1,11 @@
-import { STATUS_CODES } from "../constants/appConstants";
+import { STATUS_CODES } from '../constants/appConstants';
 
 const apiStatus = (req, res) => {
-    res.status(200).json({
-        statusCode: STATUS_CODES.OK,
-        message: 'API is working!',
-        environment: process.env.NODE_ENV,
-    })
+  res.status(200).json({
+    statusCode: STATUS_CODES.OK,
+    message: 'API is working!',
+    environment: process.env.NODE_ENV,
+  });
 };
 
 export default apiStatus;
