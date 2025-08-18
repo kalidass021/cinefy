186cdf35778fdd2f63d09212524c04a138bd232f
Kalidass <kalidass.k021@gmail.com>
2025-08-18 22:15:30 +0530

    increase rate limter limit

--- api/src/config/rateLimiter.js
+++ api/src/config/rateLimiter.js
@@ -2,7 +2,7 @@ import rateLimit from "express-rate-limit";
 
 const rateLimiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 mins
-    max: 10, // limit each IP to 100 requests per windowMS
+    max: 100, // limit each IP to 100 requests per windowMS
     message: 'Too many requests from this IP, please try again later',
 });
 
